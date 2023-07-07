549da9f0a738c0188b5ae9f9675856fd298dbab8
SumeetP96 <sumeetprajapati15@gmail.com>
2023-07-07 19:58:53 +0400

    cleanup

--- controllers/userControlles.js
+++ controllers/userControlles.js
@@ -6,7 +6,6 @@ import userService from "../services/userService.js";
  */
 const getAllUser = async (req, res) => {
   try {
-    console.log("here");
     const users = await userService.fetchAllUsers();
     res.status(200).json({ data: users });
   } catch (error) {

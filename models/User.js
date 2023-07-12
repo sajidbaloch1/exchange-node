7fa42b03554b4a9c071eea09bcf8824dbd1bb6d7
bhaumik <bhaumik677@gmail.com>
2023-07-12 11:07:48 +0400

    transaction code

--- models/User.js
+++ models/User.js
@@ -117,11 +117,25 @@ userSchema.pre("save", async function (next) {
   try {
     const hashedPassword = await encryptPassword(user.password);
     user.password = hashedPassword;
+
+    //generate transaction code
+    if (!user.transactionCode) {
+      user.transactionCode = generateTransactionCode();
+    }
+
     next();
   } catch (error) {
     next(error);
   }
 });
+
+function generateTransactionCode() {
+  const chars = "0123456789";
+  let result = "";
+  for (let i = 6; i > 0; --i)
+    result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+}
 
 const User = mongoose.model("user", userSchema);
 

c57e943fbc8b41495776fd58398fd8963e7c1f6b
pikachu <sumeet@rethinksoft.com>
2023-07-13 13:11:08 +0400

    user model refactoring

--- models/User.js
+++ models/User.js
@@ -16,9 +16,20 @@ export const USER_ROLE = {
 export const USER_ACCESSIBLE_ROLES = {
   [USER_ROLE.SYSTEM_OWNER]: Object.values(USER_ROLE),
 
-  [USER_ROLE.SUPER_ADMIN]: [USER_ROLE.ADMIN, USER_ROLE.SUPER_MASTER, USER_ROLE.MASTER, USER_ROLE.AGENT, USER_ROLE.USER],
+  [USER_ROLE.SUPER_ADMIN]: [
+    USER_ROLE.ADMIN,
+    USER_ROLE.SUPER_MASTER,
+    USER_ROLE.MASTER,
+    USER_ROLE.AGENT,
+    USER_ROLE.USER,
+  ],
 
-  [USER_ROLE.ADMIN]: [USER_ROLE.SUPER_MASTER, USER_ROLE.MASTER, USER_ROLE.AGENT, USER_ROLE.USER],
+  [USER_ROLE.ADMIN]: [
+    USER_ROLE.SUPER_MASTER,
+    USER_ROLE.MASTER,
+    USER_ROLE.AGENT,
+    USER_ROLE.USER,
+  ],
 
   [USER_ROLE.SUPER_MASTER]: [USER_ROLE.MASTER, USER_ROLE.AGENT, USER_ROLE.USER],
 
@@ -41,46 +52,13 @@ const userSchema = new mongoose.Schema(
       ref: "currency",
       required: true,
     },
-
-    isActive: {
-      type: Boolean,
-      default: true,
-    },
-    isBetLock: {
-      type: Boolean,
-      default: false,
-    },
-    isDemo: {
-      type: Boolean,
-      default: false,
-    },
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      validate: {
-        validator: async function (value) {
-          if (this._id) {
-            var count = await this.constructor.countDocuments({ _id: { $ne: this._id }, username: value });
-          } else {
-            var count = await this.model("user").countDocuments({ username: value });
-          }
-          return count === 0;
-        },
-        message: "Username already exists. Please choose a different username.",
-      },
-    },
-    fullName: {
-      type: String,
-      required: true,
-    },
-    mobileNumber: {
-      type: String,
-    },
-    city: {
-      type: String,
-    },
-
+    isActive: { type: Boolean, default: true },
+    isBetLock: { type: Boolean, default: false },
+    isDemo: { type: Boolean, default: false },
+    username: { type: String, required: true, unique: true },
+    fullName: { type: String, required: true },
+    mobileNumber: { type: String },
+    city: { type: String },
     password: { type: String, required: true },
     rate: { type: Number, min: 0, max: 1, default: 1 },
     role: {
@@ -93,10 +71,11 @@ const userSchema = new mongoose.Schema(
     clientPl: { type: Number },
     forcePasswordChange: { type: Boolean, default: false },
     transactionCode: { type: String },
-    isDeleted: { type: Boolean, default: false }, // soft delete
+    remarks: String,
   },
   { timestamps: true }
 );
+
 userSchema.plugin(timestampPlugin);
 userSchema.plugin(softDeletePlugin);
 
@@ -105,24 +84,46 @@ userSchema.path("username").validate(function (value) {
   return /^[a-zA-Z0-9_]+$/.test(value);
 }, "Username can only contain alphanumeric characters or an underscore.");
 
+// Validate username to only have alphanumeric values and underscore
+userSchema.path("username").validate(async function (value) {
+  if (this._id) {
+    var count = await this.constructor.countDocuments({
+      _id: { $ne: this._id },
+      username: value,
+    });
+  } else {
+    var count = await this.model("user").countDocuments({
+      username: value,
+    });
+  }
+  return count === 0;
+}, "Username already exists. Please choose a different username.");
+
 // Pre hook for encrypting the password
 userSchema.pre("save", async function (next) {
   const user = this;
-
-  // If the password field has not been modified, move to the next middleware
   if (!user.isModified("password")) {
     return next();
   }
-
   try {
     const hashedPassword = await encryptPassword(user.password);
     user.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
-    //generate transaction code
+// Pre hook for encrypting the password
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (!user.isModified("transactionCode")) {
+    return next();
+  }
+  try {
     if (!user.transactionCode) {
       user.transactionCode = generateTransactionCode();
     }
-
     next();
   } catch (error) {
     next(error);
@@ -132,7 +133,9 @@ userSchema.pre("save", async function (next) {
 function generateTransactionCode() {
   const chars = "0123456789";
   let result = "";
-  for (let i = 6; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+  for (let i = 6; i > 0; --i) {
+    result += chars[Math.floor(Math.random() * chars.length)];
+  }
   return result;
 }
 

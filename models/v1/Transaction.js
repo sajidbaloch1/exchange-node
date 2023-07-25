83ad2e3d7241388b96fc1f3fe963314b9162452a
Vishal Soni <vstechnology2019@gmail.com>
2023-07-25 17:03:34 +0400

    Transaction Module

---
+++ models/v1/Transaction.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import softDeletePlugin from "../plugins/soft-delete.js";
+import timestampPlugin from "../plugins/timestamp.js";
+
+const transactionSchema = new mongoose.Schema({
+  points: {
+    type: Number,
+    required: true,
+  },
+  balancePoints: {
+    type: Number,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ["credit", "debit"], // The type can only be 'credit' or 'debit'
+    required: true,
+  },
+  remark: {
+    type: String,
+    required: true,
+  },
+  fromId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user", // References the 'User' model for the user who initiated the transaction.
+    required: true,
+  },
+  toId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user", // References the 'User' model for the user who is the recipient of the transaction.
+    required: true,
+  },
+  fromtoName: {
+    type: String,
+    required: true,
+  },
+});
+
+transactionSchema.plugin(timestampPlugin);
+transactionSchema.plugin(softDeletePlugin);
+
+const Transaction = mongoose.model("transaction", transactionSchema);
+
+export default Transaction;

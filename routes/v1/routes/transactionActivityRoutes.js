a8b24538dc34e0c1ef0e63cc66a1c95b7ea1bcf7
nihang-prog <nihang.rethinksoft@gmail.com>
2023-07-27 15:36:57 +0530

    Create Transaction Api

--- routes/v1/routes/transactionActivityRoutes.js
+++ routes/v1/routes/transactionActivityRoutes.js
@@ -5,5 +5,6 @@ import { route } from "../../../lib/error-handling/routes-error-boundary.js";
 const router = express.Router();
 
 route(router, "post", "/getAllTransactionActivity", transactionActivityController.getAllTransaction);
+route(router, "post", "/createTransaction", transactionActivityController.createTransaction);
 
 export default router;

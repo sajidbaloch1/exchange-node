02788947696d8fdb8d014dc95ca28a1677124d12
pikachu <sumeet@rethinksoft.com>
2023-08-10 19:41:09 +0400

    update validation

--- requests/v1/withdrawGroupRequest.js
+++ requests/v1/withdrawGroupRequest.js
@@ -36,10 +36,10 @@ async function createWithdrawGroupRequest(req) {
   const validationSchema = Yup.object().shape({
     userId: Yup.string().required().test("userId", "Invalid userId!", isValidObjectId),
     type: Yup.string().required(),
-    remark: Yup.string().nullable(true),
-    commission: Yup.number().nullable(true),
-    minAmount: Yup.number().required(),
-    maxAmount: Yup.number().required(),
+    remark: Yup.string(),
+    commission: Yup.number(),
+    minAmount: Yup.number(),
+    maxAmount: Yup.number(),
   });
 
   await validationSchema.validate(req.body);
@@ -52,11 +52,11 @@ async function updateWithdrawGroupRequest(req) {
     _id: Yup.string().required().test("_id", "Given _id is not valid!", isValidObjectId),
     userId: Yup.string().required().test("userId", "Invalid userId!", isValidObjectId),
     type: Yup.string().required(),
-    remark: Yup.string().nullable(true),
-    commission: Yup.number().nullable(true),
-    minAmount: Yup.number().required(),
-    maxAmount: Yup.number().required(),
-    isActive: Yup.boolean().nullable(true),
+    remark: Yup.string(),
+    commission: Yup.number(),
+    minAmount: Yup.number(),
+    maxAmount: Yup.number(),
+    isActive: Yup.boolean(),
   });
 
   await validationSchema.validate(req.body);

5956064f195fd1bd60e1dd1836c586fcc2e93543
pikachu <sumeet@rethinksoft.com>
2023-08-22 20:14:20 +0400

    socket updates

--- socket/namespaces/market/broadcast.js
+++ socket/namespaces/market/broadcast.js
@@ -1,45 +1,37 @@
 import marketService from "../../../services/v1/marketService.js";
 
-const marketCallback = {
-  match_odds: marketService.getMatchOdds,
+const marketGetters = new Map().set("match_odds", marketService.getMatchOdds);
+
+const marketEmitters = new Map();
+
+export const emitMarketData = async (socket, market) => {
+  const getter = marketGetters.get(market.type);
+  if (getter) {
+    const result = await getter(market.id);
+    const data = result[0];
+    socket.to(`market:${market.id}`).emit(`market:data:${market.id}`, data);
+  }
 };
-
-let intervals = {};
-
-function removeInterval(marketId) {
-  clearInterval(intervals[marketId]);
-  delete intervals[marketId];
-}
 
 export async function startBroadcast(socket, market) {
   if (!market.id) return;
 
-  // TODO: Add a check to see if the market is open and set the interval accordingly
-
-  if (!intervals[market.id]) {
-    intervals[market.id] = setInterval(async () => {
-      const room = market.id.toString();
-      const clients = socket.adapter.rooms.get(room)?.size > 0;
-      console.log(Object.keys(intervals));
-
-      if (clients) {
-        const event = `market:${market.type}:${room}`;
-        const data = await marketCallback[market.type](market.id);
-
-        socket.to(room).emit(event, data[0]);
-      } else {
-        removeInterval(market.id);
-      }
-    }, 5000);
+  if (!marketEmitters.has(market.id)) {
+    // TODO: Add a check to see if the market is open and set the interval accordingly
+    marketEmitters.set(
+      market.id,
+      setInterval(async () => await emitMarketData(socket, market), 1000)
+    );
   }
 }
 
-export function clearEmptyIntervals(socket) {
-  Object.keys(intervals).forEach((marketId) => {
-    const room = marketId.toString();
-    const clients = socket.adapter.rooms.get(room)?.size;
-    if (clients === 0 || !intervals[marketId]) {
-      removeInterval(marketId);
+export function clearEmptyEmitters(socket) {
+  for (const [marketId, emitter] of marketEmitters.entries()) {
+    const clients = socket.adapter.rooms.get(`market:${marketId}`)?.size;
+
+    if (clients === 0) {
+      clearInterval(emitter);
+      marketEmitters.delete(marketId);
     }
-  });
+  }
 }

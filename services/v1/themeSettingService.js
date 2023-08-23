42c1b8f57a62916f4a1a47782c857a18635cf756
Vishal Soni <vishal@rethinksoft.com>
2023-08-23 12:46:46 +0400

    Return empty array

--- services/v1/themeSettingService.js
+++ services/v1/themeSettingService.js
@@ -62,7 +62,7 @@ const fetchThemeSettingId = async (userId) => {
   try {
     const existingThemeSetting = await ThemeSetting.findOne({ userId: userId });
     if (!existingThemeSetting) {
-      throw new Error("Theme Setting not found!");
+      return [];
     }
 
     // Banner Images
@@ -94,17 +94,14 @@ const fetchThemeSettingId = async (userId) => {
     );
 
     // Desktop Logo
-    const logoImage = await existingThemeSetting.getImageUrl(
-      THEME_IMAGE_TYPES.LOGO,
-      THEME_IMAGE_SIZES.LOGO.DEFAULT
-    );
+    const logoImage = await existingThemeSetting.getImageUrl(THEME_IMAGE_TYPES.LOGO, THEME_IMAGE_SIZES.LOGO.DEFAULT);
 
     const data = {
       ...existingThemeSetting._doc,
       bannerImages,
       welcomeMobileImage,
       welcomeDesktopImage,
-      logoImage
+      logoImage,
     };
 
     return data;
@@ -263,17 +260,14 @@ const getThemeSettingByCurrencyAndDomain = async ({ ...reqBody }) => {
       );
 
       // Desktop Logo
-      const logoImage = await getThemeSetting.getImageUrl(
-        THEME_IMAGE_TYPES.LOGO,
-        THEME_IMAGE_SIZES.LOGO.DEFAULT
-      );
+      const logoImage = await getThemeSetting.getImageUrl(THEME_IMAGE_TYPES.LOGO, THEME_IMAGE_SIZES.LOGO.DEFAULT);
 
       getThemeSetting = {
         ...getThemeSetting._doc,
         bannerImages,
         welcomeMobileImage,
         welcomeDesktopImage,
-        logoImage
+        logoImage,
       };
     }
     return getThemeSetting;
